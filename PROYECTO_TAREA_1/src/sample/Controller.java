5d3ffad2cf70fdad5f76de98d9f2d5d986432b05
Heutlett <adrian.araya.420@gmail.com>
2019-07-28 03:21:57 -0600

    Se termina la interfaz y el filechooser
    Se termina la interfaz y se codifica el filechooser.
    Además se configura el filechooser para filtrar solo archivos csv.

--- PROYECTO_TAREA_1/src/sample/Controller.java
+++ PROYECTO_TAREA_1/src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
-public class Controller {
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controller extends AnchorPane {
+
+    @FXML private Label lbl1;
+    @FXML private Stage primaryStage;
+
+    public void seleccionarArchivo(MouseEvent event){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File a = fileChooser.showOpenDialog(primaryStage);
+        lbl1.setText(a.getAbsolutePath());
+
+    }
+
+    public void abrirArchivo(MouseEvent event){
+
+        
+
+    }
+
 }
--- PROYECTO_TAREA_1/src/sample/Main.java
+++ PROYECTO_TAREA_1/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Tarea 1");
+        primaryStage.setScene(new Scene(root));
+
+
+
         primaryStage.show();
     }
 
